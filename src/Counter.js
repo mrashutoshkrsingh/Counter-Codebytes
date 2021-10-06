b518a972f2e06445de390504a7bc038e1b8f2d6d
Ashutosh Kumar <36168696+mrashutoshkrsingh@users.noreply.github.com>
2021-10-06 10:13:37 +0530

    Counter done

--- src/Counter.js
+++ src/Counter.js
@@ -4,7 +4,7 @@ export default function Counter() {
   const [count, setCount] = useState(0);
   const [timer, setTimer] = useState(null);
 
-  const onStartOrPauseTimer = () => {
+  const onStartOrPauseCounter = () => {
     if (!timer) {
       let newTimer = setInterval(() => {
         setCount((prevCount) => prevCount + 1);
@@ -16,7 +16,7 @@ export default function Counter() {
     }
   };
 
-  const resetTimer = () => {
+  const resetCounter = () => {
     clearInterval(timer);
     setTimer(null);
     setCount(0);
@@ -24,11 +24,11 @@ export default function Counter() {
   return (
     <div>
       <div>Counter: {count}</div>
-      <button onClick={onStartOrPauseTimer} style={{ margin: "10px" }}>
+      <button onClick={onStartOrPauseCounter} style={{ margin: "10px" }}>
         {count === 0 && !timer ? "Start" : timer ? "Pause" : "Play"}
       </button>
       {timer && count !== 0 && (
-        <button onClick={resetTimer} style={{ margin: "10px" }}>
+        <button onClick={resetCounter} style={{ margin: "10px" }}>
           Reset
         </button>
       )}
